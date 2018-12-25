a69688c7ea0439b4d040ad52afc59bfe5e5be801
Aleksandr Lapatin <a.lapatin@icloud.com>
2018-12-25 22:55:13 +0300

    Initial Commit

---
+++ BabyWatcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BabyWatcher
+//
+//  Created by macbook air on 25/12/2018.
+//  Copyright © 2018 a.lapatin@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
